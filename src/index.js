6ed0c3ace63f1f04f63e23dbad2f605bbeafa571
Chris Larsen <clarsenipod@gmail.com>
2018-10-01 15:59:58 -0400

    Add last and next months

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ class App extends React.Component {
   isLeapYear(year) {
     return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
   }
-  daysOfMonth(month, year) {
+  daysInMonth(month, year) {
     const days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     if (month === 2) {
       return this.isLeapYear(year) ? 29 : 28;
@@ -36,21 +36,50 @@ class App extends React.Component {
     const year = this.state.date.getFullYear();
     const monthName = monthNames[month];
 
-    const daysOfMonth = Array(this.daysOfMonth(month, year))
+    const lastDayOfLastMonth = new Date(
+      year,
+      month - 1,
+      this.daysInMonth(month - 1, year)
+    );
+    const firstDayOfMonth = new Date(year, month, 1);
+    const offset =
+      this.daysInMonth(month - 1, year) - lastDayOfLastMonth.getDay();
+    const daysOfLastMonth = Array(firstDayOfMonth.getDay())
       .fill()
       .map((v, i) => (
-        <li className="day-of-month" key={i}>
+        <li className="day-of-month other-month" key={i + offset}>
+          {i + offset}
+        </li>
+      ));
+
+    const daysOfThisMonth = Array(this.daysInMonth(month, year))
+      .fill()
+      .map((v, i) => (
+        <li className="day-of-month current-month" key={i + 1}>
+          {i + 1}
+        </li>
+      ));
+
+    const lastDayOfMonth = new Date(year, month, this.daysInMonth(month, year));
+    const daysOfNextMonth = Array(6 - lastDayOfMonth.getDay())
+      .fill()
+      .map((v, i) => (
+        <li className="day-of-month other-month" key={i + 1}>
           {i + 1}
         </li>
       ));
 
     return (
       <div className="App">
-        <h1>
+        <nav>
           {monthName} {year}
-        </h1>
-        <p>There are {this.daysOfMonth(month, year)} days in this month</p>
-        <ul className="current-month">{daysOfMonth}</ul>
+        </nav>
+        <p>There are {this.daysInMonth(month, year)} days in this month</p>
+        <ul className="current-month">
+          {daysOfLastMonth}
+          {daysOfThisMonth}
+          {daysOfNextMonth}
+        </ul>
       </div>
     );
   }
